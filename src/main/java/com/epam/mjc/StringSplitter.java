7424e7c7979413e671747fe5e00f26dd53d13d4c
xden78 <venera.opt@gmail.com>
2024-11-05 22:17:37 +0300

    3

--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -15,7 +15,7 @@ public class StringSplitter {
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        String[] sourceArr = source.split(Arrays.toString(delimiters.toArray()));
+        String[] sourceArr = source.split(Arrays.toString(delimiters.toArray()), 0);
         return new ArrayList<>(Arrays.asList(sourceArr));
     }
 }
